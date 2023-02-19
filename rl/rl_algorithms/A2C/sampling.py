6597fdc1e02b1ab4602e4366f1d3547434f925cf
fahmyadan <fahmy.adan14@imperial.ac.uk>
2023-02-19 14:17:11 +0000

    forward pass and action sampling

---
+++ rl/rl_algorithms/A2C/sampling.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+def select_action(state, model, SavedAction, device):
+    state = torch.from_numpy(state).float().to(device)
+    probs, state_value = model(state)
+
+    # create a categorical distribution over the list of probabilities of actions
+    m = Categorical(probs)
+
+    # and sample an action using the distribution
+    action = m.sample()
+
+    # save to action buffer
+    model.saved_actions.append(SavedAction(m.log_prob(action), state_value))
+
+    # the action to take (left or right)
+    return action.item()
